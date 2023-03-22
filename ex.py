94607e615c3732d610144d0df95d56e12c44f393
IsabelaYabe <103371464+IsabelaYabe@users.noreply.github.com>
2023-03-22 13:25:25 -0300

    Adicionar mais arquivos
    Adicionar arquivos para o restante dos exercícios

--- ex.py
+++ ex.py
@@ -7,8 +7,19 @@ from sklearn.metrics import roc_auc_score
 #Ler csv e transformar num DataFrame
 iris = pd.read_csv("./Iris.csv")
 
-print(iris["SepalLengthCm"].value_counts().sort_index())
-print(sns.countplot(x=iris["SepalLengthCm"].value_counts().sort_index()))
+'''print(iris["SepalLengthCm"].value_counts().sort_index())
+sns.countplot(x=iris["SepalLengthCm"].value_counts().sort_index())'''
+
+print(iris["Species"].value_counts())
+
+iris_setosa = iris.loc[iris["Species"] == "Iris-setosa"]
+print(iris_setosa)
+
+iris_versicolor = iris.loc[iris["Species"] == "Iris-versicolor"]
+print(iris_versicolor)
+
+iris_virginica = iris.loc[iris["Species"] == "Iris-virginica"]
+print(iris_virginica)
 
 '''print(iris["SepalLengthCm"])'''
 '''print(iris["SepalLengthCm"].value_counts())
