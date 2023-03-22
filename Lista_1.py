c37667a69aadc679720c0135b918db15e535ef8f
IsabelaYabe <103371464+IsabelaYabe@users.noreply.github.com>
2023-03-22 09:50:30 -0300

    Adicionar arquivos
    Adicionar arquivos

---
+++ Lista_1.py
@@ -0,0 +1,19 @@
+import pickle as pkl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import roc_auc_score
+#Ler csv e transformar num DataFrame
+iris = pd.read_csv("./Iris.csv")
+print(iris)
+
+print(iris["SepalLengthCm"].value_counts())
+print(type(iris["SepalLengthCm"].value_counts()))
+plot_iris = plt.hist([1,2,3,4],[10,15,20,25,30], density = True, color="g")
+print(plot_iris)
+print("foi")
+sns.countplot(x=iris["SepalLengthCm"])
+
+              
+
